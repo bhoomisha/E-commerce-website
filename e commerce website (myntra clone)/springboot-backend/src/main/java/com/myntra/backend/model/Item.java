c064cbca60754e834d9aadf8e0aabd9df1e0ee0d
BHOOMI SHARMA <bhoomisharma3104@gmail.com>
2025-11-20 19:08:52 +0530

    Update Item.java

--- e commerce website (myntra clone)/springboot-backend/src/main/java/com/myntra/backend/model/Item.java
+++ e commerce website (myntra clone)/springboot-backend/src/main/java/com/myntra/backend/model/Item.java
@@ -1,1 +1,24 @@
+package com.myntra.backend.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String company;
+    private String itemName;
+    private String image;
+    private int currentPrice;
+    private int originalPrice;
+    private int discountPercentage;
+    private int returnPeriod;
+    private String deliveryDate;
+
+}
 
