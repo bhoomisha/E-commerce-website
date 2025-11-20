064fb886d677aec5027f8f01a977ac6aa6701781
BHOOMI SHARMA <bhoomisharma3104@gmail.com>
2025-11-20 19:14:49 +0530

    Update ItemController.java

--- e commerce website (myntra clone)/springboot-backend/src/main/java/com/myntra/backend/controller/ItemController.java
+++ e commerce website (myntra clone)/springboot-backend/src/main/java/com/myntra/backend/controller/ItemController.java
@@ -1,1 +1,33 @@
+package com.myntra.backend.controller;
+
+import com.myntra.backend.model.Item;
+import com.myntra.backend.service.ItemService;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@CrossOrigin("*")
+public class ItemController {
+
+    private final ItemService service;
+
+    public ItemController(ItemService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/items")
+    public List<Item> getItems() {
+        return service.getAll();
+    }
+
+    @GetMapping("/items/{id}")
+    public Item getItem(@PathVariable Long id) {
+        return service.getOne(id);
+    }
+
+    @PostMapping("/items")
+    public Item postItem(@RequestBody Item item) {
+        return service.save(item);
+    }
+}
 
