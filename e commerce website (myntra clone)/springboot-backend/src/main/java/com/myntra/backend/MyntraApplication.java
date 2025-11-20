9b64e28b1206771fac4109faa0a48286854ff049
BHOOMI SHARMA <bhoomisharma3104@gmail.com>
2025-11-20 19:07:55 +0530

    Create MyntraApplication.java

---
+++ e commerce website (myntra clone)/springboot-backend/src/main/java/com/myntra/backend/MyntraApplication.java
@@ -0,0 +1,11 @@
+package com.myntra.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyntraApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyntraApplication.class, args);
+    }
+}
